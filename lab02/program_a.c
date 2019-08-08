d343c6a68f416a31a06ade2a4a12363cfd6182cb
akira <1144648925@qq.com>
2019-08-09 04:59:27 +0800

    lab02

---
+++ lab02/program_a.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *pi;
+
+void f(void) {
+    pi = malloc(sizeof(int));
+    *pi = 3;
+    printf("*pi is %d\n", *pi);
+    free(pi);
+}
+
+int main(void) {
+    pi = malloc(sizeof(int));
+    *pi = 4;
+    f();
+
+    return 0;
+}
---
+++ lab02/program_d.c
@@ -0,0 +1,8 @@
+int main(void) {
+    int a[20];
+    for (int i = 0;; i++) {
+        a[i] = i;
+    }
+
+    return 0;
+}
